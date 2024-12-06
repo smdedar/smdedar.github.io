4fd472b228a1e4dc6c1728267b777aeb75eb1b35
Dedar <thededar@gmail.com>
2024-12-06 20:10:26 +0600

    Update about info

--- js/about.js
+++ js/about.js
@@ -1,10 +1,10 @@
 const about = {
-    name: "Pascal Michaillat",
-    description: "I am an Associate Professor of Economics at the University of California, Santa Cruz. I research why economic slack—especially unemployment—exists, why it fluctuates over the business cycle, and how macroeconomic policies should respond to such fluctuations.",
-    image: "https://pascalmichaillat.org/picture.jpeg",
-    cv: "https://pascalmichaillat.org/cv.pdf",
-    email: "mailto:test@gmail.com",
-    linkedin: "https://www.linkedin.com/in/pascalmichaillat/",
-    github: "https://github.com/pmichaillat",
-    google_scholar: "https://scholar.google.com/citations?user=ABCDEFGHIJKLMNO",
+    name: "Dedar Alam",
+    description: "Software engineer specializing in machine learning, NLP, and data science. Skilled in Python, backend development, and scalable solutions. Passionate about leveraging technology for impactful challenges. Let's connect!",
+    image: "https://media.licdn.com/dms/image/v2/C5603AQFduRQ0wOUAZg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1623078033780?e=1738800000&v=beta&t=hsFOMnIobRRRj7sMUbO5vRS7AVPBuxH6WL8Xlvu2Qdw",
+    cv: "#",
+    email: "mailto:devded@pm.me",
+    linkedin: "https://www.linkedin.com/in/smdedar/",
+    github: "https://github.com/smdedar",
+    google_scholar: "https://scholar.google.com/citations?user=G7OR-60AAAAJ&hl=en",
 };
