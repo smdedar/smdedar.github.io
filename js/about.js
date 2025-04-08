bae7df825e417d9711cf8f738bbd7248f9a47f0c
Dedar <thededar@gmail.com>
2025-04-08 13:11:35 +0600

    docs(about): update profile image URL to a more recent one

--- js/about.js
+++ js/about.js
@@ -1,7 +1,7 @@
 const about = {
     name: "Dedar Alam",
     description: "Software engineer specializing in machine learning, NLP, and data science. Skilled in Python, backend development, and scalable solutions. Passionate about leveraging technology for impactful challenges. Let's connect!",
-    image: "https://ca.slack-edge.com/TKBMTCD45-U01TNF6EENN-9aca21f65077-512",
+    image: "https://scontent.fdac24-4.fna.fbcdn.net/v/t39.30808-6/473272444_2269542536749981_6915182046476224243_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeHxADVtgCm9BTOTK2RyaUxsDMeBQ6OvVl4Mx4FDo69WXiIlhbbUANggxiQhLn29cm0Uu2rSTauBslFP9mq653ka&_nc_ohc=8-9S2sXFfpIQ7kNvwG28lxo&_nc_oc=AdlIA7OwG-GiIbxVR9EzDTVD7bAB5CG8Sw4T0y7vqioNwpFgrRqt3lCURzLFZk5by6Y&_nc_zt=23&_nc_ht=scontent.fdac24-4.fna&_nc_gid=UpqWz-DF_GVkUP1IQrZ1zg&oh=00_AfFSnViqDZjLKOkSdXrJ8ACVqYZHgq0l0REoTmSBdFcqnA&oe=67FAAD6B",
     cv: "#",
     email: "mailto:smdedar@aol.com",
     linkedin: "https://www.linkedin.com/in/smdedar/",
