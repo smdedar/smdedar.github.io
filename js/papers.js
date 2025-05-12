8c0a60f79cd75f72c2a847155eba856fe41df21a
Dedar <thededar@gmail.com>
2025-05-12 15:38:35 +0600

    docs(papers.js): update paper descriptions and add new entry
    Update the description of an existing paper to reflect its contributions more accurately. Add a new paper entry titled "Scalable Consensus Framework with Adaptive Governance and Fault Tolerance for Blockchain Ecosystems" to the list.

--- js/papers.js
+++ js/papers.js
@@ -1,9 +1,16 @@
 const papers = [
     {
         "title": "Identifying and Analyzing Disaster-Related Tweets Through Hashtag Monitoring Using Data Mining and NLP Techniques",
-        "description": "This paper presents a method for identifying and analyzing disaster-related tweets using Natural Language Processing and Data Mining techniques. By monitoring specific hashtags, the study extracts meaningful information to improve disaster response and situational awareness.",
+        "description": "This paper proposes a model that uses hashtag monitoring, data mining, and NLP to identify disaster-related tweets and extract key information like location and severity. The system powers an API and web app that provide real-time disaster statistics and alerts via email or SMS, supporting timely emergency response.",
         "date": "2021",
         "authors": "S M Dedar Alam",
-        "url": "https://ieeexplore.ieee.org/document/9567938"
+        "url": "https://ieeexplore.ieee.org/abstract/document/9642503"
+    },
+    {
+        "title": "Scalable Consensus Framework with Adaptive Governance and Fault Tolerance for Blockchain Ecosystems",
+        "description": "This paper proposes a model that uses hashtag monitoring, data mining, and NLP to identify disaster-related tweets and extract key information like location and severity. The system powers an API and web app that provide real-time disaster statistics and alerts via email or SMS, supporting timely emergency response.",
+        "date": "2025",
+        "authors": "Saurav Ghosh, S M Dedar Alam, Batuhan Tanrikulu, Reshmi Mitra, Indranil Roy",
+        "url": "https://dl.acm.org/doi/abs/10.1145/3696673.3723058"
     }
 ];
