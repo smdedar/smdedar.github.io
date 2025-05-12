e1b40a0fdbce9cc3484e9296086cb8c8984a27fe
Dedar <thededar@gmail.com>
2025-05-12 15:28:09 +0600

    refactor(about): update profile image path to local asset
    The profile image URL was replaced with a local path to improve loading performance and ensure the image is always available, even if the external URL becomes inaccessible.

--- js/about.js
+++ js/about.js
@@ -1,7 +1,7 @@
 const about = {
     name: "Dedar Alam",
     description: "Software engineer specializing in machine learning, NLP, and data science. Skilled in Python, backend development, and scalable solutions. Passionate about leveraging technology for impactful challenges. Let's connect!",
-    image: "https://scontent.fdac24-4.fna.fbcdn.net/v/t39.30808-6/473272444_2269542536749981_6915182046476224243_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeHxADVtgCm9BTOTK2RyaUxsDMeBQ6OvVl4Mx4FDo69WXiIlhbbUANggxiQhLn29cm0Uu2rSTauBslFP9mq653ka&_nc_ohc=WgQFmSQy4FoQ7kNvwEWy_U9&_nc_oc=AdkKDUkCc-GVNYTJjnCe7qk09Rp9FW0R4bZT9Fyl-8NBAYc8Eivd-hlCjMi62-d7VdM&_nc_zt=23&_nc_ht=scontent.fdac24-4.fna&_nc_gid=rQmMscVnO9N0t3BvI6iRGQ&oh=00_AfG5n0nCkSrbuaAqe1p-LC46sFRHgKVvZK2CKvfDjfBK_w&oe=6809D6AB",
+    image: "asset/profile.jpg",
     cv: "#",
     email: "mailto:smdedar@aol.com",
     linkedin: "https://www.linkedin.com/in/smdedar/",
