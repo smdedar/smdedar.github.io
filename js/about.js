016e339ef405e1ab077a4a42facf529a0fd3d839
Dedar <thededar@gmail.com>
2025-11-03 13:15:20 +0300

    Update profile image url

--- js/about.js
+++ js/about.js
@@ -1,8 +1,8 @@
 const about = {
     name: "Dedar Alam",
     description: "Software engineer specializing in machine learning, NLP, and data science. Skilled in Python, backend development, and scalable solutions. Passionate about leveraging technology for impactful challenges. Let's connect!",
-    image: "https://media.licdn.com/dms/image/v2/D5603AQHbDUioTfSTxw/profile-displayphoto-scale_200_200/B56ZlOat02HQAY-/0/1757957251111?e=1761177600&v=beta&t=mZY0YbA-u5Sfct2SbqzzDYE3oYmTqIcV7y13rKrEgwk",
-    cv: "https://ggl.link/smdedar",
+    image: "https://dl.dropboxusercontent.com/scl/fi/yuba28muq67gu43h3ny7q/profile.jpeg?rlkey=arof407r5upkw42wpj9r696xt",
+    cv: "",
     email: "mailto:smdedar@aol.com",
     linkedin: "https://www.linkedin.com/in/smdedar/",
     github: "https://github.com/smdedar",
